f417c832371e471f3d09a7a5c0d6f8b79be6cddb
AnnaMarczakiewicz <akarasiewicz@gmail.com>
2025-10-06 14:38:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,2 @@
 # Wyświetlenie wyników
 # Wyświetlenie wyników
-# Wyświetlenie wyników
-dystans = float(input("Podaj drogę pokonaną przez samochód (w km): "))
-spalanie = float(input("Podaj średnie spalanie paliwa (litry na 100 km): "))
-
-# Stała cena paliwa
-cena_paliwa = 6.5  # zł za litr
-
-# Obliczenie zużycia paliwa
-zuzycie_paliwa = (spalanie / 100) * dystans
-
-# Obliczenie kosztu podróży
-koszt_podrozy = zuzycie_paliwa * cena_paliwa
-
-# Wyświetlenie wyników
-print("Przewidywane zużycie paliwa w litrach:", zuzycie_paliwa)
-print("Szacowany koszt podróży w zł:", koszt_podrozy)
-# test
